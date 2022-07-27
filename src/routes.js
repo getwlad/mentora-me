392235eea9d2fb2b5c0ab1ec2c06f5996f062821
Isabel <belfferraz@gmail.com>
2022-07-27 14:48:29 -0300

    Arquivo CRUD Mentor adicionado

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
+
 import studentValidator from "./middlewares/studentValidator.js";
 import studentController from "./app/controllers/student/StudentController.js";
+
+import mentorValidator from "./middlewares/mentorValidator";
+import MentorController from "./app/controllers/mentor/MentorController";
 
 const routes = new Router();
 
@@ -13,5 +17,11 @@ routes.get("/students/:id", studentController.show);
 routes.post("/students", studentValidator, studentController.create);
 routes.put("/students/:id", studentValidator, studentController.update);
 routes.delete("/students/:id", studentController.delete);
+
+routes.get("/controllers", MentorController.list);
+routes.get("/controllers/:id", MentorController.show);
+routes.post("/controllers", mentorValidator, MentorController.create);
+routes.put("/controllers:id", mentorValidator, MentorController.update);
+routes.delete("/controllers:id", MentorController.delete);
 
 export default routes;
