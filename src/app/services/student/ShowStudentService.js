b01d250f8035101e7ad9ebb3285ea28d00e17f56
getwlad <wladmir.rrodrigues@gmail.com>
2022-07-27 22:09:26 -0300

    res pra quando nao achar alguem por id e atualizado db

--- src/app/services/student/ShowStudentService.js
+++ src/app/services/student/ShowStudentService.js
@@ -4,6 +4,9 @@ const ShowStudentService = {
   showStudentData: (studentId) => {
     const studentShow = ListStudentService.listStudentsService();
     const student = studentShow.find((item) => item.id === studentId);
+    if (!student) {
+      return { Erro: "Mentorado não encontrado" };
+    }
     return student;
   },
 };
