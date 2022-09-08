d90efbd64c012e03ee9ecdc52a389290f6d6f1d7
getwlad <wladmir.rrodrigues@gmail.com>
2022-09-08 09:34:06 -0300

    ajustando para deploy

--- src/app.js
+++ src/app.js
@@ -11,6 +11,19 @@ class App {
   }
   middlewares() {
     this.server.use(express.json());
+    this.server.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,OPTIONS,POST,PUT, DELETE"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+      );
+      next();
+    });
   }
   routes() {
     this.server.use(routes);
